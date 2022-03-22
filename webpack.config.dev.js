fe97d8e3882880ce48b476d2674d9c18626f532f
Ernesto Ormeño <eormeno12@gmail.com>
2022-03-22 17:38:09 -0500

    Project ready for production.

---
+++ webpack.config.dev.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const DotenvWebpack = require('dotenv-webpack')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, './src/utils/'),
+            '@templates': path.resolve(__dirname, './src/templates/'),
+            '@styles': path.resolve(__dirname, './src/styles/'),
+            '@images': path.resolve(__dirname, './src/assets/images/'),
+        }
+    },
+    module: {
+        rules: [
+            // Babel
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            // CSS
+            {
+                test: /\.css|.styl$/i,
+                use: [MiniCSSExtractPlugin.loader, 
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            // Images
+            {
+                test: /\.png/,
+                type: 'asset/resource'
+            },
+            // Fonts
+            {
+                test: /\.(woff|woff2)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[hash][ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCSSExtractPlugin({
+            filename: 'assets/styles/[name].[contenthash].css'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src', 'assets/images'),
+                    to: 'assets/images'
+                }
+            ]
+        }),
+        new DotenvWebpack(),
+    ]
+};
